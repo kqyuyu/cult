7b20abd5a02b0cf887455fbd3b27a98b89579940
kqyuyu <91009127+kqyuyu@users.noreply.github.com>
2024-12-18 03:15:48 +0700

    Add files via upload

--- page6/javas.js
+++ page6/javas.js
@@ -1,7 +1,7 @@
 const imageList = ['images/234.png', 'images/123.png', 'images/egg.png'];
 const img = document.querySelector('.img');
 const next = document.getElementById('next');
-let numImage = 0; // Используем let для лучшей практики
+let numImage = 0;
 
 img.src = imageList[numImage];
 
